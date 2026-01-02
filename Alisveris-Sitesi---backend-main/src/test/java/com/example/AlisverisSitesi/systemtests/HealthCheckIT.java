e07d7f7f3af7491aca6858a6177614638bda2a19
fatmagul-yilmaz <fatmagullyilmazz.fy@gmail.com>
2026-01-02 15:42:31 +0300

    bittiinşş

--- Alisveris-Sitesi---backend-main/src/test/java/com/example/AlisverisSitesi/systemtests/HealthCheckIT.java
+++ Alisveris-Sitesi---backend-main/src/test/java/com/example/AlisverisSitesi/systemtests/HealthCheckIT.java
@@ -1,24 +1,28 @@
 package com.example.AlisverisSitesi.systemtests;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class HealthCheckIT {
 
+    @BeforeEach
+    void setup() {
+        // RestAssured'a varsayılan 8080 yerine 8084'ü kullanmasını söylüyoruz
+        RestAssured.port = 8084;
+        RestAssured.baseURI = "http://localhost";
+    }
+
     @Test
     void backend_should_be_running() {
-
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8084;
-
         RestAssured
             .given()
             .when()
             .get("/actuator/health")
             .then()
-            .statusCode(200)
+            .statusCode(200) // Artık 8084/actuator/health'e gideceği için 200 dönecek
             .body("status", equalTo("UP"));
     }
 }
