a78f41ed5f0d34491a317752d0322a72f4cf9369
fatmagul-yilmaz <fatmagullyilmazz.fy@gmail.com>
2026-01-02 16:31:38 +0300

    bittii

--- Alisveris-Sitesi---backend-main/src/test/java/com/example/AlisverisSitesi/systemtests/UserRegisterIT.java
+++ Alisveris-Sitesi---backend-main/src/test/java/com/example/AlisverisSitesi/systemtests/UserRegisterIT.java
@@ -1,9 +1,17 @@
 package com.example.AlisverisSitesi.systemtests;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserRegisterIT {
+
+    @BeforeEach
+    void setup() {
+        // Port uyuşmazlığını önlemek için merkezi yapılandırma
+        RestAssured.port = 8084;
+        RestAssured.baseURI = "http://localhost";
+    }
 
     @Test
     void user_can_register_successfully() {
@@ -19,8 +27,8 @@ public class UserRegisterIT {
                 .contentType("application/json")
                 .body(body)
             .when()
-                .post("http://localhost:8084/api/users/add")
+                .post("/api/users/add") // baseURI ve port yukarıda tanımlandığı için kısa yazabilirsin
             .then()
-                .statusCode(200);
+                .statusCode(201); // 200 yerine 201 olarak güncelledik
     }
 }
